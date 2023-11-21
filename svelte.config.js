8e56d1c00ce3808e9a7cf74f7f67a475629626ac
Christoph Dötzer <christoph.doetzer@posteo.de>
2023-11-21 18:11:22 +0100

    updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
         paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-        }
+            // change below to your repo name
+            base: dev ? "" : "/bwinf-42-zusatzaufgabe",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte"
     }
 };
+
 export default config;
